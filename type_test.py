3730881ba32c76cdcb11e7ab101ffa52da14595f
최유진 <yjyj0774@gmail.com>
2024-09-10 10:05:46 +0900

    두번째 예제 description

--- type_test.py
+++ type_test.py
@@ -1,6 +1,10 @@
 # 타입검사기
 # 사용할 메서드 : input print type
 
+# 노드켜줘 앱
+# input으로 '노드'라는 단어가 포험되면 cli REPL 환경인 node가 실행되도록 명령
+# '타입'이라는 단어가 포함되면 타입 검사
+
 def type_test():
     try:
         text = int(input("input : "))
