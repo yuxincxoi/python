398133756c6263eef5273437a2dfa690e74f1da9
최유진 <yjyj0774@gmail.com>
2024-09-10 10:03:46 +0900

    type 검사기 생성

--- type_test.py
+++ type_test.py
@@ -1,2 +1,11 @@
 # 타입검사기
 # 사용할 메서드 : input print type
+
+def type_test():
+    text = input("input : ")
+    if type(text) == str:
+        print("문자열입니다.")
+    elif type(text) == int:
+        print("int입니다.")
+        
+type_test()
