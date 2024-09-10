d0940643c5439c6db3e01f0ea6b121433b06497a
최유진 <yjyj0774@gmail.com>
2024-09-10 11:52:03 +0900

    나의 데이터

---
+++ 20.py
@@ -0,0 +1,10 @@
+budget = {
+    "lunch":9000, 
+    "amusement park ticket": 30000  ,
+    "snacks": 8000,
+    "drinks": 4000,
+    "dinner": 14000,
+    "round-train ticket": 64000,
+    "room": 60000,
+    "bus ticket": 6000
+}
